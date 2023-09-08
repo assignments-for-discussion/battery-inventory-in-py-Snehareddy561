9c8cbd6157a257a589a6f48f550069404df269a2
Snehareddy561 <144459085+Snehareddy561@users.noreply.github.com>
2023-09-08 21:52:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ def count_batteries_by_health(present_capacities):
     "failed": 0
   }
   
-    for capacity in present_capacities:
-        if capacity >= 100:
-            counts["healthy"] += 1
-        elif 80 <= capacity < 100:
-            counts["exchange"] += 1
-        else:
-            counts["failed"] += 1
+  for capacity in present_capacities:
+     if capacity >= 100:
+         counts["healthy"] += 1
+     elif 80 <= capacity < 100:
+         counts["exchange"] += 1
+     else:
+         counts["failed"] += 1
         
-   return counts
+  return counts
   
 def test_bucketing_by_health():
-    print("Counting batteries by health...\n")
-    present_capacities = [115, 118, 80, 95, 91, 77]
-    counts = count_batteries_by_health(present_capacities)
+  print("Counting batteries by health...\n")
+  present_capacities = [115, 118, 80, 95, 91, 77]
+  counts = count_batteries_by_health(present_capacities)
   
-    assert counts["exchange"] == 3
-    assert counts["failed"] == 1
-    assert counts["healthy"] == 2
+  assert counts["exchange"] == 3
+  assert counts["failed"] == 1
+  assert counts["healthy"] == 2
   
-    print("Done counting :)")
+  print("Done counting :)")
 
 
 test_bucketing_by_health()
